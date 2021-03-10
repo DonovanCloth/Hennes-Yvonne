1681aff3c7d6ef861ea58c381c0139f01a965cfe
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2021-03-10 15:47:30 +0100

    nach besprechung

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   let horizontalLine = document.querySelector(".horizontal-line");
-  let body = document.querySelector("body");
 
-  let timeOut;
-
-  function resetLine() {
-   
-    horizontalLine.style.transitionDuration = "1s";
-    horizontalLine.style.transform = `translateX(0px)`;
-  }
-
-  body.addEventListener("mousemove", (e) => {
-    clearTimeout(timeOut);
-    let clientX = e.clientX;
-    horizontalLine.style.transitionDuration = "0.4s";
-    horizontalLine.style.transform = `translateX(${ (clientX ) * -1}px)`;
-
-    timeOut = setTimeout(() => {
-      resetLine();
-    }, 3000);
-  });
+  horizontalLine.style.transform = "translateX(0px)";
+ 
 });
