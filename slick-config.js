54b02e037fac0cfa3a186ee15f774cf64156f2d1
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2021-03-10 14:37:54 +0100

    initial commit

---
+++ slick-config.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $(".fade").slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    speed: 1000,
+    fade: true,
+    cssEase: "linear",
+    dragable: true,
+  });
+});
