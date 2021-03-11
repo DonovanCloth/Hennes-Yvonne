7716574922b130f76164e784f0168c192d2098bf
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2021-03-11 15:13:18 +0100

    sent to client

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
   let horizontalLine = document.querySelector(".horizontal-line");
-
   horizontalLine.style.transform = "translateX(0px)";
- 
 });
